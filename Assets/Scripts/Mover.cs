c3489954acb477f1a68c5948096f6aa70bbb7dee
Rick Dennison <rdennison12@gmail.com>
2026-02-27 04:13:42 -0500

    Adding project to version control

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+    
+    private void UpdateAnimator()
+    {
+     Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+     Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+     float speed = localVelocity.z;
+     // ReSharper disable once Unity.PreferAddressByIdToGraphicsParams
+     GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RPG
+
+## Commits
+* Initial commit
+* Adding project to version control
